decbcc3d1dc17d3c271c3e0c6f0cd15c3bc57dc7
pkncoder <alex.h.preston@gmail.com>
2026-01-31 12:00:28 -0500

    Added constants around RobotPeriodic

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,6 +2,15 @@ package frc.robot;
 
 public class Constants {
 
+    public static class ComputerConstants {
+        public static final int primaryPort = 0;
+    }
+
+    public static class DriveConstants {
+        public static final double driveSpeedMultiplier = 1.0;
+        public static final double rotationSpeedMultiplier = 1.0;
+    }
+
     public static class ShooterConstants {
         public static final int shooterMotorID = 0;
     }
