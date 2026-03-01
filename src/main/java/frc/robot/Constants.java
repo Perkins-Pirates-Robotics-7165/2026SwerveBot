bd0cadc5ca91f32951bd14b416af1b12d0004b27
pkncoder <alex.h.preston@gmail.com>
2026-03-01 10:06:13 -0500

    Added limit switch logic for the wall bed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -76,9 +76,14 @@ public class Constants {
         public static final int wallBedMainMotorID = 6;
         public static final int wallBedFollowerMotorID = 7;
 
+        // Limit switch ports
+        public static final int wallBedLeftLimitSwitchPort = 0;
+        public static final int wallBedRightLimitSwitchPort = 1;
+
         // Motor base speeds
         public static final double wallBedRaiseSpeed = 0.1;
         public static final double wallBedLowerSpeed = -0.1;
+
     }
 
     // Constants for the limelight subsystem
--- src/main/java/frc/robot/subsystems/WallBedSubsystem.java
+++ src/main/java/frc/robot/subsystems/WallBedSubsystem.java
@@ -6,6 +6,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.WallBedConstants;
 
@@ -14,6 +15,10 @@ public class WallBedSubsystem extends SubsystemBase {
     // The 2 motors on either side of the robot, controlling entire intake frame (wall bed)
     private final TalonFX wallBedMainMotor = new TalonFX(WallBedConstants.wallBedMainMotorID);
     private final TalonFX wallBedFollowerMotor = new TalonFX(WallBedConstants.wallBedFollowerMotorID);
+
+    // Two limit switches to make sure the wall bed doesn't move too far
+    private final DigitalInput wallBedLeftLimitSwitch = new DigitalInput(WallBedConstants.wallBedLeftLimitSwitchPort);
+    private final DigitalInput wallBedRightLimitSwitch = new DigitalInput(WallBedConstants.wallBedRightLimitSwitchPort);
 
     // Initializer, use to set configurations and set attributes
     public WallBedSubsystem() {
@@ -30,12 +35,44 @@ public class WallBedSubsystem extends SubsystemBase {
     }
 
     /*
-     * Move the wall bed
+     * Move the wall bed, if either of the limit switches are pressed, no movement will occour
      * 
      * @param speed - Wall Bed motor drive speed [-1.0, 1.0]
      */
     public void moveWallBedMotor(double speed) {
-        wallBedMainMotor.set(speed);
+
+        // If both of the limit switches are not pressed, set speed as normal (this logic is using De Morgan's laws)
+        if (!(wallBedLeftLimitSwitch.get() || wallBedRightLimitSwitch.get())) {
+            wallBedMainMotor.set(speed);
+        } 
+        
+        // If one or two of the limit switches are pressed, only move if the speed is negitive (lowering the wall bed)
+        else if (speed < 0.0) {
+            wallBedMainMotor.set(speed);
+        }
+
+        // If neither, do not move the wall bed
+        else {
+            wallBedMainMotor.set(0.0);
+        }
+    }
+
+    /*
+     * Returns the status of the left limit switch
+     * 
+     * @returns boolean - True if pressed, false if not pressed
+     */
+    public boolean getLeftWallBedLimitSwitch() {
+        return wallBedLeftLimitSwitch.get();
+    }
+
+    /*
+     * Returns the status of the right limit switch
+     * 
+     * @returns boolean - True if pressed, false if not pressed
+     */
+    public boolean getRightWallBedLimitSwitch() {
+        return wallBedRightLimitSwitch.get();
     }
 
     // Period function on field, called every 20ms
