a6a85deaafc2e05254f997a58b0bc37dc84e33d3
pkncoder <alex.h.preston@gmail.com>
2026-03-01 00:40:23 -0500

    Warning

--- src/main/java/frc/robot/commands/ApproachFunnel.java
+++ src/main/java/frc/robot/commands/ApproachFunnel.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ProgramaticCommandConstants.ApproachFunnelConstants;
 import frc.robot.Utilities.MathUtilities;
